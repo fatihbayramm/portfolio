21145b4ee6f16a993d6227e55d45dec699e30228
fatih bayram <fatihbayramybs@gmail.com>
2024-11-21 12:42:41 +0300

    header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "port-grey": "#e1ecec",
+        "port-melon": "#ffd369",
+        "port-blue": "#17a9c7",
+      },
+    },
   },
   plugins: [],
 };
